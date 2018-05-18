ae47b7abdac33f6c0d94d87187239d2b7d95c2aa
BKertesz <kertesz.balazs92@gmail.com>
2018-05-18 20:38:22 +0100

    Added tests for store

--- models/specs/store_spec.js
+++ models/specs/store_spec.js
@@ -1,23 +1,55 @@
 const Store = require('../store.js');
 const assert = require('assert');
+const Record = require('../record.js')
 
 describe('Store',function(){
 
-  beforeEach(function(){
+  let store;
+  let record;
 
+  beforeEach(function(){
+    store = new Store('My Store');
+    record = new Record({
+      title: 'Their Greatest Hits 1971 - 1975',
+      artist: 'Eagles',
+      genre: 'rock',
+      price: 1000
+    });
   })
 
-  it('should have a name');
+  it('should have a name',function(){
+    const actual = store.name;
+    assert.deepStrictEqual(actual,"My Store")
+  });
 
-  it('should start with no funds');
+  it('should start with no funds',function(){
+    const actual = store.funds;
+    assert.deepStrictEqual(actual,0);
+  });
 
-  it('should be able to add funds');
+  it('should be able to add funds',function(){
+    store.addFunds(100);
+    const actual = store.funds;
+    assert.deepStrictEqual(actual,100)
+  });
 
-  it('should start with an empty collection of records');
+  it('should start with an empty collection of records',function(){
+    const actual = store.records.length;
+    assert.deepStrictEqual(actual,0)
+  });
 
-  it('should be able to add a record to its stock');
+  it('should be able to add a record to its stock',function(){
+    store.addRecord(record);
+    const actual = store.records.length;
+    assert.deepStrictEqual(actual,1);
+  });
 
-  it('should be able to remove a record from its stock');
+  it('should be able to remove a record from its stock',function(){
+    store.addRecord(record);
+    store.removeRecord(findRecordByTitle('Their Greatest Hits 1971 - 1975'));
+    const actual = store.records.length;
+    assert.deepStrictEqual(actual,0);
+  });
 
   it('should be able to sell a record if it has the record');
 
--- models/store.js
+++ models/store.js
@@ -1,5 +1,29 @@
 class Store{
 
+  constructor(name){
+    this.name = name;
+    this.funds = 0;
+    this.records = [];
+  }
+
+  addFunds(amount){
+    this.funds += amount;
+  }
+
+  addRecord(record){
+    this.records.push(record);
+  }
+
+  findRecordByTitle(title){
+    return this.records.filter(record => record.title == title)[0];
+  }
+
+  removeRecord(record){
+    const position = this.records.indexOf(record);
+    this.records.splice(position,1);
+  }
+
+
 }
 
 
